b5ae2d25c12b9d67679d68c3c1ad9d286004e2cf
erickfrodrigues <erickfrodrigues@hotmail.com>
2024-09-09 20:52:46 -0300

    parte2

---
+++ ApiCursoLinqCanal/Controllers/AlimentoController.cs
@@ -0,0 +1,21 @@
+﻿using ApiCursoLinqCanal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCursoLinqCanal.Controllers
+{
+    public class AlimentoController : Controller
+    {
+
+        [HttpGet]
+        [Route("/ObterAlimentos")]    
+        public object ObterAlimentos()
+        {
+            //jeito 1 
+            Alimentos alimentos = new();
+            return alimentos.PopularDados();
+
+            //jeito 2
+            //return new Alimentos().PopularDados();
+        }
+    }
+}

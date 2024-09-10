ed3292a5e9c8afe137fdecbab1ee693db83ada28
erickfrodrigues <erickfrodrigues@hotmail.com>
2024-09-09 22:01:12 -0300

    max mib by

--- ApiCursoLinqCanal/Controllers/AlimentoController.cs
+++ ApiCursoLinqCanal/Controllers/AlimentoController.cs
@@ -120,5 +120,35 @@ namespace ApiCursoLinqCanal.Controllers
 
         }
 
+
+        [HttpGet]
+        [Route("/AlimentoMenosCalorico")]
+        public object AlimentoMenosCalorico()
+        {
+            //para filtrar informaçoes vamos utilizar o where 
+            //lambda sao expressoes para darmos nomes aos nosso atributos - exemplo -> letra => e traz o campo exemplo s=>s.calorias
+
+            //jeito 1 
+            Alimentos alimentos = new();
+            //return alimentos.PopularDados().GroupBy(s => s.Calorias);
+            return alimentos.PopularDados().MinBy(s => s.Calorias);
+
+        }
+
+
+        [HttpGet]
+        [Route("/AlimentoMaisCalorico")]
+        public object AlimentoMaisCalorico()
+        {
+            //para filtrar informaçoes vamos utilizar o where 
+            //lambda sao expressoes para darmos nomes aos nosso atributos - exemplo -> letra => e traz o campo exemplo s=>s.calorias
+
+            //jeito 1 
+            Alimentos alimentos = new();
+            //return alimentos.PopularDados().GroupBy(s => s.Calorias);
+            return alimentos.PopularDados().MaxBy(s => s.Calorias);
+
+        }
+
     }
 }

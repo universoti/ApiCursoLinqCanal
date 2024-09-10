324f6f03e2a1300179f5ce77b5319db057220bc8
erickfrodrigues <erickfrodrigues@hotmail.com>
2024-09-09 21:48:28 -0300

    min max

--- ApiCursoLinqCanal/Controllers/AlimentoController.cs
+++ ApiCursoLinqCanal/Controllers/AlimentoController.cs
@@ -62,6 +62,63 @@ namespace ApiCursoLinqCanal.Controllers
 
         }
 
+        [HttpGet]
+        [Route("/AgruparKakAlimento")]
+        public object AgruparKakAlimento()
+        {
+            //para filtrar informaçoes vamos utilizar o where 
+            //lambda sao expressoes para darmos nomes aos nosso atributos - exemplo -> letra => e traz o campo exemplo s=>s.calorias
+
+            //jeito 1 
+            Alimentos alimentos = new();
+            return alimentos.PopularDados().GroupBy(s => s.Calorias);
+            
+
+        }
+
+        [HttpGet]
+        [Route("/AgruparUnidadeAlimento")]
+        public object AgruparUnidadeAlimento()
+        {
+            //para filtrar informaçoes vamos utilizar o where 
+            //lambda sao expressoes para darmos nomes aos nosso atributos - exemplo -> letra => e traz o campo exemplo s=>s.calorias
+
+            //jeito 1 
+            Alimentos alimentos = new();
+            //return alimentos.PopularDados().GroupBy(s => s.Calorias);
+            return alimentos.PopularDados().GroupBy(s => s.Unidade);
+
+        }
+
+
+        [HttpGet]
+        [Route("/AlimentoMenosCalorico2")]
+        public object AlimentoMenosCalorico2()
+        {
+            //para filtrar informaçoes vamos utilizar o where 
+            //lambda sao expressoes para darmos nomes aos nosso atributos - exemplo -> letra => e traz o campo exemplo s=>s.calorias
+
+            //jeito 1 
+            Alimentos alimentos = new();
+            //return alimentos.PopularDados().GroupBy(s => s.Calorias);
+            return alimentos.PopularDados().Min(s => s.Calorias);
+
+        }
+
+
+        [HttpGet]
+        [Route("/AlimentoMaisCalorico2")]
+        public object AlimentoMaisCalorico2()
+        {
+            //para filtrar informaçoes vamos utilizar o where 
+            //lambda sao expressoes para darmos nomes aos nosso atributos - exemplo -> letra => e traz o campo exemplo s=>s.calorias
+
+            //jeito 1 
+            Alimentos alimentos = new();
+            //return alimentos.PopularDados().GroupBy(s => s.Calorias);
+            return alimentos.PopularDados().Max(s => s.Calorias);
+
+        }
 
     }
 }
